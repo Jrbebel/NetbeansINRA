c1c5fc3287b2f71f3143a2f42b6c93e1a00695a7
bebel <bebel@PortableLive>
2013-04-30 12:09:26 -0400

    30/03/2013 agentcontroller

--- src/Inra2013/urzBundle/Controller/AgentController.php
+++ src/Inra2013/urzBundle/Controller/AgentController.php
@@ -144,25 +144,31 @@ class AgentController extends Controller {
         $user = $this->container->get('security.context')->getToken()->getUser()->getFonction(); // on récupere la fonction de l'utilisateur connecté
         $userId = $this->container->get('security.context')->getToken()->getUser()->getId(); // on récupere la fonction de l'utilisateur connecté
         $StatusProto = $this->getDoctrine()->getEntityManager()->getRepository('Inra2013urzBundle:Protocole')->StatusEncours();
+        $StatusFini=$this->getDoctrine()->getEntityManager()->getRepository('Inra2013urzBundle:Protocole')->StatusTermine() ;
         $StatusProtoId = $this->getDoctrine()->getEntityManager()->getRepository('Inra2013urzBundle:Protocole')->StatusEncoursId($userId);
-       
+    //   \Doctrine\Common\Util\Debug::dump($StatusFini);
         if ($this->get('security.context')->isGranted('ROLE_ADMINISTRATEUR')) {
+       
+
             return $this->render("Inra2013urzBundle:Default:IndexAdmin.html.twig");
+            
         } elseif ($this->get('security.context')->isGranted('ROLE_RESPONSABLE')) {
 
             if ($user == "Laborantin(e)") {
 
-                return $this->render("Inra2013urzBundle:Default:IndexUser.html.twig", array('response' => $StatusProto));
+                return $this->render("Inra2013urzBundle:Default:IndexUser.html.twig", array('response' => $StatusProto,'responseFini' => $StatusFini,'User'=>$user));
                 
             } else if ($user == "Chercheur") {
 
                 return $this->render("Inra2013urzBundle:Default:IndexChercheur.html.twig", array('response' => $StatusProtoId));
             }
+       
         } elseif ($this->get('security.context')->isGranted('ROLE_UTILISATEUR')) {
 
             if ($user == "Laborantin(e)") {
 
-                return $this->render("Inra2013urzBundle:Default:IndexUser.html.twig", array('response' => $StatusProto));
+                return $this->render("Inra2013urzBundle:Default:IndexUser.html.twig", array('response' => $StatusProto,'responseFini' => $StatusFini));
+           
             } else if ($user == "Chercheur") {
 
    return $this->render("Inra2013urzBundle:Default:IndexChercheur.html.twig", array('response' => $StatusProtoId));
